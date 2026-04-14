f5f4a00a83f298bd2503cc4771ec622d12d58b93
Jomari Garces <jomarigarces.work@gmail.com>
2026-04-14 02:09:09 -0400

    Test

--- Transition/convex-client.js
+++ Transition/convex-client.js
@@ -11,7 +11,7 @@
 // ============================================================
 // CONFIGURE THIS: Paste your Convex deployment URL here
 // ============================================================
-const CONVEX_URL = "https://successful-bee-546.convex.cloud";
+const CONVEX_URL = "https://joyous-ant-26.convex.cloud";
 // ============================================================
 
 import { ConvexClient } from "https://cdn.jsdelivr.net/npm/convex@1.13.0/browser/+esm";
